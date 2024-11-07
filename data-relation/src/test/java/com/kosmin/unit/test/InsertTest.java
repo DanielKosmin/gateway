39758cc06514bfcc23811642cc30aa7591229bb9
Daniel Kosmin <kosmindaniel@gmail.com>
2024-11-07 09:41:56 -0500

    adding remaning test cases

--- data-relation/src/test/java/com/kosmin/unit/test/InsertTest.java
+++ data-relation/src/test/java/com/kosmin/unit/test/InsertTest.java
@@ -1,5 +1,7 @@
 package com.kosmin.unit.test;
 
+import com.kosmin.exception.CheckingInsertionException;
+import com.kosmin.model.Request;
 import com.kosmin.model.repository.CheckingModel;
 import com.kosmin.model.repository.CreditModel;
 import com.kosmin.repository.insert.InsertCheckingRecords;
@@ -8,6 +10,7 @@ import com.kosmin.repository.query.QueryPrimaryKey;
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,7 +40,7 @@ public class InsertTest extends BaseUnitTest {
   }
 
   @Test
-  @DisplayName("insert records into checking table")
+  @DisplayName("asynchronously insert records into checking table")
   void insertRecordsIntoCheckingTable() {
     CheckingModel bankingAccountModel =
         CheckingModel.builder()
@@ -61,7 +64,7 @@ public class InsertTest extends BaseUnitTest {
   }
 
   @Test
-  @DisplayName("insert records into credit table")
+  @DisplayName("asynchronously insert records into credit table")
   void insertRecordsIntoCreditTable() {
     CreditModel creditCardRecordsModel =
         CreditModel.builder()
@@ -85,5 +88,56 @@ public class InsertTest extends BaseUnitTest {
     Assertions.assertDoesNotThrow(
         () -> insertCreditRecords.insertCreditRecords(creditCardRecordsModel));
     Mockito.verify(namedParameterJdbcTemplate, Mockito.times(1)).update(insertCreditQuery, params);
+  }
+
+  @Test
+  @DisplayName("synchronously insert records into checking records")
+  void insertRecordsIntoCheckingRecords() {
+    CheckingModel bankingAccountModel =
+        CheckingModel.builder()
+            .transactionDescription("")
+            .transactionDate(Date.valueOf("2024-10-10"))
+            .transactionType("")
+            .transactionAmount(BigDecimal.valueOf(0.0))
+            .balance(BigDecimal.valueOf(0.0))
+            .build();
+    final Map<String, Object> params = new HashMap<>();
+    params.put("transactionDescription", bankingAccountModel.getTransactionDescription());
+    params.put("transactionDate", bankingAccountModel.getTransactionDate());
+    params.put("transactionType", bankingAccountModel.getTransactionType());
+    params.put("transactionAmount", bankingAccountModel.getTransactionAmount());
+    params.put("balance", bankingAccountModel.getBalance());
+    Mockito.when(namedParameterJdbcTemplate.update(insertCheckingQuery, params)).thenReturn(0);
+    Assertions.assertThrows(
+        CheckingInsertionException.class,
+        () -> insertCheckingRecords.insertCheckingRecords(bankingAccountModel, true));
+    Mockito.verify(namedParameterJdbcTemplate, Mockito.times(1))
+        .update(insertCheckingQuery, params);
+  }
+
+  @Test
+  @DisplayName("synchronously insert records into credit table")
+  void insertRecordsIntoCreditRecords() {
+    var creditReq =
+        Request.CreditRecordPayload.builder()
+            .creditTransactionDate("2024-10-05")
+            .creditTransactionType("sale")
+            .creditTransactionCategory("test")
+            .creditTransactionAmount(200.00)
+            .build();
+    Request request =
+        Request.builder()
+            .creditRecords(List.of(creditReq))
+            .checkingTransactionDescription("withdrawal")
+            .checkingTransactionDate("2024-11-06")
+            .checkingTransactionAmount(200.00)
+            .checkingBalance(200.00)
+            .build();
+    Mockito.when(namedParameterJdbcTemplate.update(Mockito.eq(insertCreditQuery), Mockito.anyMap()))
+        .thenReturn(0);
+    var res = insertCreditRecords.insertCreditRecords(request);
+    Assertions.assertNotNull(res);
+    Assertions.assertEquals(res.size(), 1);
+    Assertions.assertEquals(res.get(0), creditReq);
   }
 }
--- data-relation/src/test/java/com/kosmin/unit/test/QueryTest.java
+++ data-relation/src/test/java/com/kosmin/unit/test/QueryTest.java
@@ -1,8 +1,17 @@
 package com.kosmin.unit.test;
 
+import com.kosmin.exception.PrimaryKeyNotFoundException;
 import com.kosmin.repository.query.QueryPrimaryKey;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class QueryTest extends BaseUnitTest {
@@ -18,48 +27,40 @@ public class QueryTest extends BaseUnitTest {
     queryPrimaryKey = new QueryPrimaryKey(namedParameterJdbcTemplate, sqlQueriesConfig);
   }
 
-  //  @Test
-  //  @DisplayName("test querying parent id and throwing exception when result is not found")
-  //  void testQueryThrows() {
-  //    Map<String, Object> params =
-  //        Map.of(
-  //            "checkingTransactionDescription",
-  //            "%transactionDescription%",
-  //            "checkingTransactionType",
-  //            "test",
-  //            "checkingEndDate",
-  //            Date.valueOf("2024-01-01"),
-  //            "checkingStartDate",
-  //            Date.valueOf("2024-12-31"));
-  //    Mockito.doReturn(Collections.emptyList())
-  //        .when(namedParameterJdbcTemplate)
-  //        .query(Mockito.eq(query), Mockito.eq(params), Mockito.<RowMapper<Integer>>any());
-  //    Assertions.assertThrows(
-  //        PrimaryKeyNotFoundException.class,
-  //        () ->
-  //            queryPrimaryKey.queryPrimaryKey(
-  //                "test", "2024-12-31", "2024-01-01", "transactionDescription"));
-  //  }
+  @Test
+  @DisplayName("test querying valid parent request id")
+  void testValidParentRequestId() {
+    String startDate = "2024-11-01";
+    String endDate = "2024-11-30";
+    Map<String, Object> params =
+        Map.of(
+            "checkingStartDate",
+            java.sql.Date.valueOf(startDate),
+            "checkingEndDate",
+            java.sql.Date.valueOf(endDate));
+    Mockito.doReturn(List.of(1))
+        .when(namedParameterJdbcTemplate)
+        .query(Mockito.eq(query), Mockito.eq(params), Mockito.<RowMapper<Integer>>any());
+    var res = queryPrimaryKey.queryPrimaryKey(startDate, endDate);
+    Assertions.assertEquals(res, 1);
+  }
 
-  //  @Test
-  //  @DisplayName("test querying parent id")
-  //  void testQuery() {
-  //    Map<String, Object> params =
-  //        Map.of(
-  //            "checkingTransactionDescription",
-  //            "%transactionDescription%",
-  //            "checkingTransactionType",
-  //            "test",
-  //            "checkingEndDate",
-  //            Date.valueOf("2024-01-01"),
-  //            "checkingStartDate",
-  //            Date.valueOf("2024-12-31"));
-  //    Mockito.doReturn(List.of(1))
-  //        .when(namedParameterJdbcTemplate)
-  //        .query(Mockito.eq(query), Mockito.eq(params), Mockito.<RowMapper<Integer>>any());
-  //    Assertions.assertEquals(
-  //        queryPrimaryKey.queryPrimaryKey(
-  //            "test", "2024-12-31", "2024-01-01", "transactionDescription"),
-  //        1);
-  //  }
+  @Test
+  @DisplayName("test querying invalid parent request id")
+  void testInvalidParentRequestId() {
+    String startDate = "2024-11-01";
+    String endDate = "2024-11-30";
+    Map<String, Object> params =
+        Map.of(
+            "checkingStartDate",
+            java.sql.Date.valueOf(startDate),
+            "checkingEndDate",
+            java.sql.Date.valueOf(endDate));
+    Mockito.doReturn(Collections.EMPTY_LIST)
+        .when(namedParameterJdbcTemplate)
+        .query(Mockito.eq(query), Mockito.eq(params), Mockito.<RowMapper<Integer>>any());
+    Assertions.assertThrows(
+        PrimaryKeyNotFoundException.class,
+        () -> queryPrimaryKey.queryPrimaryKey(startDate, endDate));
+  }
 }
